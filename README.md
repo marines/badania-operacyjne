0f559dbe5716b6ccf417d2ab7042f9b573853f2d
pawel <git@pawelgorka.com>
2015-08-26 13:40:05 +0200

    adding gulp with watching on sources and auto rebuilding of gitbook, should be immune to gitbook build errors

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Badania Operacyjne
 Opracowanie
 
 ---
+ 
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
 
-gulp.task('default', function() {
-  console.log('Hello world.');
+gulp.task('build', function () {
+  return gulp.src('*.js', {read: false})
+      .pipe(plumber())
+      .pipe(shell('gitbook build'));
 });
+
+gulp.task('serve', function () {
+  browserSync({server: './_book'});
+  gulp.watch('**/*.md', ['build']);
+  gulp.watch(["**/*.html"]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'serve']);
